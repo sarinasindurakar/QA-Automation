346a59ebeabd83fcc7db14b38fdbb91384a8b968
sarinasindurakar <sarinasindurakar82@gmail.com>
2022-11-28 10:05:20 +0545

    Screenshots and Navigation

--- IATI_POM/src/test/java/Base/BaseTest.java
+++ IATI_POM/src/test/java/Base/BaseTest.java
@@ -3,10 +3,17 @@ package Base;
 import Pages.*;
 import com.github.javafaker.Faker;
 import jdk.jfr.Description;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import java.io.File;
+import java.io.IOException;
 
 public class BaseTest {
     private WebDriver driver;
@@ -31,6 +38,14 @@ public class BaseTest {
         activitydetailpage=new ActivityDetailPage(driver);
         descriptionPage=new DescriptionPage(driver);
         fakedata=new Faker();
+
+    }
+    @BeforeMethod
+    public void record() throws IOException {
+        TakesScreenshot ts=(TakesScreenshot)driver;
+        File src=ts.getScreenshotAs(OutputType.FILE);
+        File target=new File("src/screenshots/test2.png");
+        FileUtils.copyFile(src,target);
 
     }
 
